01c3c043f6a486495f09a471c6fb99803d8aba52
xiejunzhuo <1263156186@qq.com>
2019-12-27 22:12:59 +0800

    Initial commit

---
+++ src/main/java/com/study/demo/oom/Client.java
@@ -0,0 +1,21 @@
+package com.study.demo.oom;
+
+/**
+ *
+ * Exception in thread "main" java.lang.StackOverflowError
+ *
+ * 递归调用，深度调用导致吧栈撑爆了
+ * @author 谢君卓
+ * @version 1.0
+ * @date 2019/12/27 21:09
+ */
+public class Client {
+    public static void main(String[] args) {
+        genObject();
+    }
+
+    private static void genObject(){
+        genObject();
+    }
+
+}
